c63756e6a745298a9c45fdbdf27411cd9c51fdf7
DeveloperNuel <egentle05@gmail.com>
2018-11-09 17:48:54 +0200

    Adding routes Folder

---
+++ api/routes/parcels.js
@@ -0,0 +1,142 @@
+import express from 'express';
+import Parcels from '../db/parcels';
+import User from '../db/users';
+
+const router = express.Router();
+
+// Create parcel
+router.post('/', (req, res) => {
+  const { userId, parcel } = req.body;
+  const priceNormal = 20; // Price in $
+  const priceExpress = 70;
+  const priceIsForParcelAreaLimit = 2706; // Area is in cm
+  // area of a parcel
+  const area =
+    parcel.height * parcel.length * 2 +
+    parcel.width * parcel.height * 2 +
+    parcel.length * parcel.width * 2;
+
+  // Parcel price in $
+  let price;
+
+  const user = User.filter(customer => customer.userId === userId)[0];
+
+  const calculatePrice = pricePerOneWeight => {
+    // Parcel area is not exceed
+    if (area <= priceIsForParcelAreaLimit) {
+      return pricePerOneWeight * parcel.weight * parcel.quantity;
+    }
+    // Parcel area exceed
+    return (
+      (pricePerOneWeight * parcel.weight * parcel.quantity * area) /
+      priceIsForParcelAreaLimit
+    );
+  };
+
+  if (user) {
+    // Price per Service on Normal
+    // Price per Service on Express
+    price =
+      parcel.From === parcel.To
+        ? calculatePrice(priceNormal)
+        : calculatePrice(priceExpress);
+    // Parcel Id
+    const pId = Parcels.length + 1;
+    const parcelWith = parcel;
+    parcelWith.parcelId = pId;
+    parcelWith.status = 'booking';
+
+    // Save parcel
+    Parcels.push({ userId, price, parcel: parcelWith });
+
+    console.log(Parcels);
+    return res.status(201).json({
+      success: true,
+      message: 'Parcel created successfully',
+      parcelId: pId,
+      price: Number(price.toFixed(2))
+    });
+  }
+
+  return res.status(404).json({
+    success: false,
+    message: 'User not Exist'
+  });
+});
+
+// Get all parcels
+router.get('/', (req, res) => {
+  const parcelDeliveries = [];
+  console.log(Parcels);
+  Parcels.map(parcel => parcelDeliveries.push(parcel.parcel));
+  res.status(200).json({
+    success: true,
+    message: 'Parcels retrieved successfully',
+    parcels: parcelDeliveries
+  });
+});
+
+// Get a specific parcels
+router.get('/:parcelId', (req, res) => {
+  const parcelDelivery = Parcels.filter(
+    parcel => parcel.parcel.parcelId === Number(req.params.parcelId)
+  );
+
+  if (parcelDelivery.length > 0) {
+    return res.status(200).json({
+      success: true,
+      message: `Parcel of id ${req.params.parcelId} retrieved successfully`,
+      parcel: parcelDelivery[0]
+    });
+  }
+  return res.status(404).json({
+    success: false,
+    message: `Parcel of id ${req.params.parcelId} does not exist`
+  });
+});
+
+// Cancel a parcel delivery order
+router.put('/:parcelId/cancel', (req, res) => {
+  let parcelDelivery = {};
+  let index;
+  req.params.parcelId = Number(req.params.parcelId);
+  for (let i = 0; i < Parcels.length; i += 1) {
+    if (Parcels[i].parcel.parcelId === req.params.parcelId) {
+      parcelDelivery = Parcels[i].parcel;
+      index = i;
+      break;
+    }
+  }
+
+  if (parcelDelivery) {
+    if (
+      parcelDelivery.status === 'peding' ||
+      parcelDelivery.status === 'canceled'
+    ) {
+      parcelDelivery.status = 'canceled';
+      if (Parcels[index].parcel.parcelId === parcelDelivery.parcelId) {
+        Parcels[index].parcel.status = 'canceled';
+        return res.status(200).send({
+          success: true,
+          message: 'Parcel is successfully canceled',
+          parcel: parcelDelivery
+        });
+      }
+      return res.status(405).send({
+        success: false,
+        message: 'Data index changed. Try again!'
+      });
+    }
+    return res.status(405).send({
+      success: false,
+      message:
+        'Not allowed to cancel a parcel with status of delivered or transit'
+    });
+  }
+  return res.status(404).send({
+    success: false,
+    message: 'Parcel not found'
+  });
+});
+
+export default router;
---
+++ api/routes/users.js
@@ -0,0 +1,109 @@
+import express from 'express';
+import User from '../db/users';
+import Parcels from '../db/parcels';
+
+const router = express.Router();
+
+
+router.post('/register', (req, res) => {
+  const errors = [];
+
+  if (req.body.password !== req.body.passwordConfirm) {
+    errors.push({
+      text: 'Password do not match'
+    });
+  }
+
+  if (req.body.password.length < 6) {
+    errors.push({
+      text: 'Passwor must be at least 6 characters'
+    });
+  }
+
+  if (req.body.names.length < 3) {
+    errors.push({
+      text: 'Enter valid Names'
+    });
+  }
+
+  if (
+    req.body.email.indexOf('@') === -1 ||
+    req.body.email.indexOf('.') === -1
+  ) {
+    errors.push({
+      text: 'Enter valid Email'
+    });
+  }
+
+  if (errors.length > 0) {
+    res.status(400).send(errors);
+  } else {
+    const oldUser = User.filter(user => user.email === req.body.email);
+    if (oldUser.length > 0) {
+      return res.status(400).send({
+        success: false,
+        message: 'Email already registered'
+      });
+    }
+    try {
+      const newUser = {
+        userId: User.length + 1,
+        names: req.body.names,
+        email: req.body.email,
+        password: req.body.password
+      };
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(newUser.password, salt);
+      newUser.password = hash;
+      User.push(newUser);
+
+      return res.status(201).send({
+        success: true,
+        message: 'User is successfully registered'
+      });
+    } catch (err) {
+      res.send({
+        success: false,
+        message: `Failed to register, error: ${err} `
+      });
+    }
+  }
+  return res.send({
+    success: false,
+    message: 'User not registered! Please try again'
+  });
+});
+
+// Get user parcel delivery orders
+router.get('/:userId/parcels', (req, res) => {
+  const userParcels = [];
+  req.params.userId = Number(req.params.userId);
+
+  const user = User.filter(eachUser => eachUser.userId === req.params.userId);
+  if (user.length > 0) {
+    Parcels.forEach(parcel => {
+      if (parcel.userId === req.params.userId) {
+        userParcels.push(parcel.parcel);
+      }
+    });
+
+    if (userParcels.length > 0) {
+      return res.status(200).json({
+        success: true,
+        message: 'User parcels retrieved successfully',
+        userId: req.params.userId,
+        parcels: userParcels
+      });
+    }
+    return res.status(404).json({
+      success: true,
+      message: 'User has no parcels'
+    });
+  }
+  return res.status(404).json({
+    success: false,
+    message: 'User not Exist'
+  });
+});
+
+module.exports = router;
