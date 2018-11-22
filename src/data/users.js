b9088afe5058df3ad23a1c37109d8876df03cf42
DeveloperNuel <egentle05@gmail.com>
2018-11-22 14:39:24 +0200

    [Feature #161841284] Remove hard coded data.

--- src/data/users.js
+++ src/data/users.js
@@ -1,16 +1,3 @@
-const users = [
-  {
-    userId: 1,
-    names: 'Emmanuel TUYISHIMIRE',
-    email: 'egentle05@gmail.com',
-    password: 'jesus',
-  },
-  {
-    userId: 2,
-    names: 'Gedeon Mugisha',
-    email: 'mugisha@gmail.com',
-    password: 'jesus',
-  },
-];
+const users = [];
 
 export default users;
